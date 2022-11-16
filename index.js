a4c6e11d20b14b11652be788946ac574256afe5d
ianK-code <kian99564@gmail.com>
2022-11-16 16:05:41 +0300

    Will come to it later

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+    const result = a + b
+    return result;
+}
+function subtract(a, b){
+    const result = a - b
+    return result;
+}
+function multiply(a, b){
+    const result = a * b
+    return result;
+}
+function divide(a, b){
+    const result = a / b
+    return result;
+}
+// returns NaN as appropriate
+function makeInt(){
+    const parsed =parseInt("random", 10);
+    return parsed
+}
+function makeInt(){
+    const n = 2.125
+    const parsed =parseInt(`${n}`, 10);
+    return parsed
+}
+// function add(){
+// }
+// function add(){
+// }
